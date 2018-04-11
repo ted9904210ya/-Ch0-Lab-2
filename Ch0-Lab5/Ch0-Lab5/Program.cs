4764d2f053bd220ce3f19443e0f0aad1387107cf
ted <ted9904210ya@gmail.com>
2018-04-11 14:57:42 +0800

    Project update

---
+++ Ch0-Lab5/Ch0-Lab5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch0_Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is Ch0-Lab5,my name is Ted.");
+        }
+    }
+}
