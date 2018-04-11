b1cc17ac07e381ca0ba659df0d196c236cd2aeb2
ted <ted9904210ya@gmail.com>
2018-04-11 15:18:20 +0800

    Project update

--- Project1/Ch0-Lab4/Program.cs
+++ Project1/Ch0-Lab4/Program.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ch0_Lab5
+namespace Ch0_Lab4
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is Ch0-Lab5,my name is Ted.");
+            Console.WriteLine("HELLO");
         }
     }
 }
