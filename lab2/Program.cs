00ba35ea0734aee2e75b2034ad927215824e5b0c
ted <ted9904210ya@gmail.com>
2018-04-11 13:44:08 +0800

    Complete Lab1

---
+++ lab2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            Console.WriteLine(a);
+        }
+    }
+}
